7627d45a70d1eb5586c46ff68be00ff767b6a07d
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-02 21:51:02 +0200

    Create header.h

---
+++ lab/lab3/header.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <conio.h>
+#include <vector>
+using namespace std;
+
+struct Fraction;
+
+int Task_1();
+void ChainFract(Fraction, float);
+
+int Task_15();
