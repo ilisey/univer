aad238c7f76f0a7e90e5523cd88fca0c2675cd51
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-02 21:44:38 +0200

    Create header.h

---
+++ lab/lab1/header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+int Task_9();
+int Task_1();
+bool isPrime(int n);
