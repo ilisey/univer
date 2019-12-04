6525b9b5a913c816c654c4b333b3b802b7690923
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-04 14:18:18 +0200

    Create MyLib.h

---
+++ №6/MyLib.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int Series_2();
+int Series_13();
+int Series_21();
+int Series_23();
+
+void RectPS(double x1, double y1, double x2, double y2, double P, double S);
+int Proc_5();
+void SortInc3(double A, double B, double C);
+int Proc_12();
+int RootsCount(double A, double B, double C);
+int Proc_17();
+float radtodeg(float r);
+int Proc_33();
+int MinMax_23();
+int MinMax_16();
+int MinMax_10();
+int MinMax_26();
