8e1fab06dcd39b40f3b4a1588a093f0691dae1df
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-04 14:23:26 +0200

    Create MyLib.h

---
+++ Matrix/MyLib.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int Matrix_2();
+int Matrix_10();
+int Matrix_15();
+int Matrix_20();
+int Matrix_35();
+int Matrix_46();
+int Matrix_50();
+int Matrix_65();
+int Matrix_62();
+int Matrix_83();
