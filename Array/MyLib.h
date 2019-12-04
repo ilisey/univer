033cd575f3704a4f02537bcf03dc05c7eb2b9d61
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-04 14:21:19 +0200

    Create MyLib.h

---
+++ Array/MyLib.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int Array5();
+int Array10();
+int Array15();
+int Array20();
+int Array26();
+int Array39();
+int Array52();
+int Array57();
+int Array63();
+int Array70();
+int Array100();
+int Array108();
+int Array112();
+int Array117();
+int Array125();
+int Array130();
+int Array132();
+int Array135();
+int Array138();
