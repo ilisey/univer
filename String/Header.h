8b84b22b5e7750c97158834da33b2807bb49ef2d
ilisey <45861320+ilisey@users.noreply.github.com>
2019-12-04 14:25:33 +0200

    Create Header.h

---
+++ String/Header.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+# include <windows.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int String_4();
+int String_6();
+int String_11();
+int String_17();
+int String_23();
+int String_24();
+int String_26();
+int String_30();
+int String_39();
+int String_45();
+int String_48();
+int String_53();
+int String_62();
+int String_63();
+int String_66();
